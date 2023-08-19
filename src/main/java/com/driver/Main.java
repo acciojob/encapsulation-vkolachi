80863fd84b8d06c3f814f4961533c69b14b91779
vkola <vkolachi@gmail.com>
2023-08-19 16:29:05 +0530

    1st attemp

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly obj =new RWOnly();
-        obj.name="raj";
+       // obj.name="raj";
 
 
         obj.setName("raj");
